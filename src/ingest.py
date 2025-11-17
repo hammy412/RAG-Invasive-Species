cf48315253470a15ddab597d50135d8256d6ff0e
Aidan <132329147+hammy412@users.noreply.github.com>
2025-11-17 14:44:39 -0500

    html

--- src/ingest.py
+++ src/ingest.py
@@ -1,9 +1,12 @@
 import os
 from bs4 import BeautifulSoup
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import PyPDFLoader, WebBaseLoader, TextLoader
+from langchain_community.document_loaders import PyPDFLoader, WebBaseLoader, TextLoader, BSHTMLLoader
+from trafilatura import extract
+from langchain_core.documents import Document
 
-RAW_DIR = "data_raw/"
+
+RAW_DIR = "html/"
 OUT_PATH = "data_processed/chunks.json"
 
 def load_documents():
@@ -14,6 +17,9 @@ def load_documents():
         if f.lower().endswith(".pdf"):
             loader = PyPDFLoader(RAW_DIR + f)
             docs.extend(loader.load())
+        if f.lower().endswith(".html"):
+            docs.extend(load_clean_html(os.path.join("html", f)))
+
 
     return docs
 
@@ -24,6 +30,16 @@ def chunk_documents(docs):
         chunk_overlap=150
     )
     return splitter.split_documents(docs)
+
+def load_clean_html(path):
+     with open(path, "r", encoding="utf-8") as f:
+        html = f.read()
+
+        text = extract(html, include_comments=False, include_tables=True)
+        if not text:
+            return []
+
+        return [Document(page_content=text, metadata={"source": path})]
 
 
 if __name__ == "__main__":
